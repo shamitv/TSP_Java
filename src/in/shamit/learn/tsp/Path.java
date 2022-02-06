144f06073fcf8a204d2a33d1c6b10b22ecf6cf90
Shamit Verma <github@shamit.in>
2022-02-06 16:27:12 +0530

    Reduced calls to Collections

--- src/in/shamit/learn/tsp/Path.java
+++ src/in/shamit/learn/tsp/Path.java
@@ -1,124 +1,129 @@
 package in.shamit.learn.tsp;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Path {
-    Path parentPath=null;
+    Path parentPath = null;
+    Set<Vertex> vertices = new HashSet<>();
+    Edge edge = null;
 
     public Path() {
     }
 
     public Path(Path parentPath) {
         this.parentPath = parentPath;
-    }
-
-    Edge edge = null;
-    public boolean doesIncludeVertex(Vertex v){
-        if(edge != null && edge.getDestination().equals(v) ){
-            return true;
+        if (parentPath != null) {
+            vertices.addAll(parentPath.getVertices());
         }
-        if(parentPath != null){
-            return parentPath.doesIncludeVertex(v);
-        }
-        return false;
-    }
-
-    public boolean canAddEdge(Edge e){
-        if( edge != null &&
-                (e.getDestination().equals(edge.getDestination()) || e.getDestination().equals(edge.getOrigin()))){
-            return false;
-        }
-        if(parentPath != null){
-            return parentPath.canAddEdge(e);
-        }
-        return true;
     }
 
 
-    public int getCost(){
+    public boolean doesIncludeVertex(Vertex v) {
+        return vertices.contains(v);
+    }
+
+    public boolean canAddEdge(Edge e) {
+        return !vertices.contains(e.getDestination());
+    }
+
+
+    public int getCost() {
         int cost = 0;
-        if(edge !=null){
+        if (edge != null) {
             cost += edge.getWeight();
         }
-        if(parentPath != null){
+        if (parentPath != null) {
             cost += parentPath.getCost();
         }
         return cost;
     }
 
-    public void setEdge(Edge e){
-        edge = e;
+    public void setEdge(Edge e) {
+        if (edge == null) {
+            edge = e;
+            vertices.add(e.getOrigin());
+            vertices.add(e.getDestination());
+        } else {
+            throw new RuntimeException("Edge is Read only. Can't be modified once set");
+        }
+
     }
 
 
     public Vertex getLastVertex() {
-        if(edge==null){
-            if(parentPath != null){
+        if (edge == null) {
+            if (parentPath != null) {
                 return parentPath.getLastVertex();
             }
             return null;
-        }else{
+        } else {
             return edge.getDestination();
         }
     }
 
     public int edgeCount() {
         int count = 0;
-        if(edge !=null){
+        if (edge != null) {
             count++;
         }
-        if(parentPath!=null){
-            count +=parentPath.edgeCount();
+        if (parentPath != null) {
+            count += parentPath.edgeCount();
         }
         return count;
     }
 
-    public int getParentCount(){
+    public int getParentCount() {
         int count = 0;
-        if(parentPath!=null){
+        if (parentPath != null) {
             count++;
             count += parentPath.getParentCount();
         }
         return count;
     }
 
-    public String getEdgesAsString(){
+    public String getEdgesAsString() {
         StringBuilder ret = new StringBuilder();
 
-        if(parentPath!=null){
-            List<String> parentStrs= new ArrayList<>();
+        if (parentPath != null) {
+            List<String> parentStrs = new ArrayList<>();
             Path parent = this.parentPath;
-            while(parent!=null){
+            while (parent != null) {
                 parentStrs.add(parent.getEdgesAsStringExcludeParent());
                 parent = parent.parentPath;
             }
-            for(int i=parentStrs.size();i>0;i--){
-                ret.append(parentStrs.get(i-1)  + "|");
+            for (int i = parentStrs.size(); i > 0; i--) {
+                ret.append(parentStrs.get(i - 1) + "|");
             }
         }
         ret.append(getEdgesAsStringExcludeParent());
         return ret.toString();
     }
 
-    String getEdgesAsStringExcludeParent(){
+    String getEdgesAsStringExcludeParent() {
         StringBuilder ret = new StringBuilder();
-        if(edge!=null){
+        if (edge != null) {
             ret.append(edge + " ");
         }
         return ret.toString();
     }
 
-    Path getRootParent(){
-        if(parentPath==null){
+    Path getRootParent() {
+        if (parentPath == null) {
             return this;
-        }else {
+        } else {
             Path ret = parentPath;
-            while(ret.parentPath!=null){
+            while (ret.parentPath != null) {
                 ret = ret.parentPath;
             }
             return ret;
         }
+    }
+
+    public Set<Vertex> getVertices() {
+        return vertices;
     }
 
     @Override
--- src/in/shamit/learn/tsp/Traverse.java
+++ src/in/shamit/learn/tsp/Traverse.java
@@ -11,36 +11,26 @@ import java.util.logging.Logger;
 public class Traverse {
     public static Logger log = Logger.getLogger("Traversal");
     Path lowestCostPath = null;
-    public Path DoTraverse(Graph g,Path p){
-        if(p==null){
-            p = new Path();
-        }
+
+    public Path DoTraverse(Graph g, Path p) {
 
         Vertex start = g.getFirstVertex();
-        Vertex lastVertex = p.getLastVertex();
-        if(lastVertex == null){
-            lastVertex=start;
-        }
-        final Vertex lv = lastVertex;
-        Path finalP = p;
+        Vertex lastVertex = (p == null) ? start : p.getLastVertex();
 
-
-
-        g.vertices.keySet().parallelStream().forEach(v_id->{
+        g.vertices.keySet().stream().parallel().forEach(v_id -> {
             Vertex v = g.vertices.get(v_id);
-            if(!finalP.doesIncludeVertex(v) ){
-                if(!v.equals(lv)){
-                    Path childPath = new Path(finalP);
-                    Edge e = lv.getEdgeToVertex(v);
-                    if(childPath.canAddEdge(e)){
+
+            if (!v.equals(lastVertex)) {
+                Path childPath = new Path(p);
+                Edge e = lastVertex.getEdgeToVertex(v);
+                if (childPath.canAddEdge(e)) {
+                    childPath.setEdge(e);
+                    DoTraverse(g, childPath);
+                } else {
+                    if (start.equals(v)) {
                         childPath.setEdge(e);
-                        DoTraverse(g,childPath);
-                    }else{
-                        if(start.equals(v)){
-                            if(isPathComplete(g,childPath,v)){
-                                childPath.setEdge(e);
-                                evaluatePath(childPath);
-                            }
+                        if (isPathComplete(g, childPath)) {
+                            evaluatePath(childPath);
                         }
                     }
                 }
@@ -48,33 +38,50 @@ public class Traverse {
         });
         return null;
     }
+
     AtomicLong pathCount = new AtomicLong(0);
+
     void evaluatePath(Path p) {
         long curCount = pathCount.incrementAndGet();
-        if(lowestCostPath == null || (lowestCostPath.getCost() > p.getCost())){
+        if (lowestCostPath == null || (lowestCostPath.getCost() > p.getCost())) {
             lowestCostPath = p;
-            log.info("New Path, Cost = " +p.getCost());
+            log.info("New Path, Cost = " + p.getCost());
             System.out.println(p.getCost() + " " + p);
         }
-        if(curCount % 1000_000_00 == 0){
-            logProgress(curCount,p);
+        if (curCount % 1000_000_00 == 0) {
+            logProgress(curCount, p);
         }
     }
 
     Instant prevInstant = Instant.now();
+
     private void logProgress(long curCount, Path p) {
         Instant cur = Instant.now();
-        String timeElapsed = Duration.between(prevInstant,cur).toSeconds() + " Seconds | ";
-        log.info(curCount + " Time Elapsed " + timeElapsed + " Current Cost + Path  | " + p.getCost() + " | " +p);
+        String timeElapsed = Duration.between(prevInstant, cur).toSeconds() + " Seconds | ";
+        log.info(curCount + " Time Elapsed " + timeElapsed + " Current Cost + Path  | " + p.getCost() + " | " + p);
         prevInstant = cur;
     }
 
-    boolean isPathComplete(Graph g, Path p,Vertex nextVertex){
+    boolean isPathComplete(Graph g, Path p, Vertex nextVertex) {
         boolean complete = false;
         Vertex fist = g.getFirstVertex();
-        if(nextVertex.equals(fist)){
+        if (nextVertex.equals(fist)) {
             int verticesInGraph = g.vertices.size();
-            if(p.edgeCount()==verticesInGraph-1){
+            if (p.getVertices().size() == verticesInGraph - 1) {
+                complete = true;
+            }
+        }
+        return complete;
+    }
+
+    boolean isPathComplete(Graph g, Path p) {
+        boolean complete = false;
+        Vertex fist = g.getFirstVertex();
+        Vertex last = p.getLastVertex();
+        if (last.equals(fist)) {
+            int verticesInGraph = g.vertices.size();
+            int verticesInPath = p.getVertices().size();
+            if (verticesInPath == verticesInGraph) {
                 complete = true;
             }
         }
@@ -87,11 +94,11 @@ public class Traverse {
 
         log.setLevel(Level.INFO);
         String filePath = Config.DATA_FILE;
-        log.info("Reading "+filePath);
+        log.info("Reading " + filePath);
         Graph g = GraphReader.readFromXML(filePath);
 
         Traverse t = new Traverse();
-        t.DoTraverse(g,null);
+        t.DoTraverse(g, null);
         log.info("done");
     }
 }
