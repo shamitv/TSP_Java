3fb1d54ef63e91179cb849151b3f68d743dc01bc
Shamit Verma <github@shamit.in>
2022-02-06 12:54:24 +0530

    Maintenance

--- src/in/shamit/learn/tsp/Config.java
+++ src/in/shamit/learn/tsp/Config.java
@@ -1,6 +1,6 @@
 package in.shamit.learn.tsp;
 
 public class Config {
-    public static String DATA_DIR="/Users/shamit/work/algo/tsp/data/";
+    public static String DATA_DIR="/Users/shamit/work/algo/tsp/TSP_Java/data/";
     public static String DATA_FILE = DATA_DIR + "br17.xml";
 }
